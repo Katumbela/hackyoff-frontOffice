0a09c97b183b8ba695c7508c3ff6a6c44e428f19
João Afonso Katombela <ja3328173@gmail.com>
2024-05-25 19:03:04 +0100

    adit navbar datas home route

--- src/presentation/components/navbar/navbar.tsx
+++ src/presentation/components/navbar/navbar.tsx
@@ -24,8 +24,7 @@ export function NavBar() {
  
     };
 
-    window.addEvent
-    Listener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
       window.removeEventListener("scroll", handleScroll);
